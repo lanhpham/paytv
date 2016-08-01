f5a630671543016c117583780f63a981721933aa
lanhpham <honglanhtoantin09@gmail.com>
2016-08-01 23:11:31 +0700

    Combine 100 to 1

--- Giam_roi_mang/test1.py
+++ Giam_roi_mang/test1.py
@@ -1,55 +1,48 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import timeit
 import glob
+import pandas as pd
 import threading
+import timeit
 import numpy as np
-path =r'/data/out_put'
-allFiles = glob.glob(path + "/*.txt")
-#%%
-data = pd.DataFrame()
-#mylist = []
+path =r"/Users/sondinh/Downloads/bf_only_vectordays/group/" # use your path
+allFiles = glob.glob(path + "/*.csv")
 #%%
 #for file_ in allFiles:
-#    df = pd.read_csv(file_,index_col=None, header= None)
-    #maxscore = pd.DataFrame(df.ix[:,0:].max())
-    #mylist.append(df)
-    #data = pd.concat(df, ignore_index = True)
-#%%
-#maxscore  = pd.DataFrame(data.ix[:,0:].max())
-#%%
-#print len(allFiles)
-#%%
+#    df = pd.read_csv(file_)
+#    if len(result) == 0:
+#        result = df
+#    else:
+#        result = pd.concat([df,result] )
 class myThread(threading.Thread):
-    def __init__(self, my_range, allFiles):
+    def __init__(self, my_range, allFiles, index):
         threading.Thread.__init__(self)
         self.my_range = my_range
         self.allFiles = allFiles
-        self.mylist = []
+        self.index = index
     def run(self):
-        self.cond.acquire()
+        result = []
         for i in self.my_range:
-            for j in self.allFiles:
-                df = pd.read_csv(file_,index_col=None, header= None)
-                self.list.append(df)
-    def getResult(self):
-        self.cond.acquire() # <--
-        while not self.done: #  <--
-            self.cond.wait()#  <--  We're waiting that self.done becomes True
-        self.cond.release() #  <--
-        return self.mylist
+            df = pd.read_csv(self.allFiles[i],index_col=None)
+            if (len(result) == 0):
+                result = df
+            else:
+                result = pd.concat([df,result],ignore_index = True)
+        result.to_csv("/Users/sondinh/Downloads/bf_only_vectordays/sum/" + str(self.index) +  ".csv" ,index = False)    
 start = timeit.default_timer()        
 length_data = range(len(allFiles))
-data_split = np.array_split(length_data,10)
+data_split = np.array_split(length_data,1)
 
 threads = []
 for index, item in enumerate(data_split, start=0):
-    threads.append(myThread(item, allFiles))
+    threads.append(myThread(item, allFiles, index))
 arr_result = []
 # Start new Threads
 for t in threads:
     t.start()
 for t in threads:
     t.join()
-for t in threads:
-    arr_result.append(t.getResult())
+stop = timeit.default_timer()
+print stop - start
+print "Exiting Main Thread"
+#%%
+sum = pd.read_csv("/Users/sondinh/Downloads/bf_only_vectordays/sum/0.csv")
