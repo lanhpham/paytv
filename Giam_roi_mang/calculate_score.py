35212d22ad14cd537da85d80a36f135c4ead6148
lanhpham <honglanhtoantin09@gmail.com>
2016-08-09 22:16:50 +0700

    Read by chunk

--- Giam_roi_mang/calculate_score.py
+++ Giam_roi_mang/calculate_score.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import glob
 import timeit 
-path =r'/data/output_Lanh/Group'
-allFiles = glob.glob(path + "/*.txt")
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
+#%%
+#define variable
+CHUNKSIZE = 1000
+path =r'/Users/sondinh/Downloads/result'
+allFiles = glob.glob(path + "/*.txt")
 #%%
 #df2 = pd.read_csv("/data/output_Lanh/Group/result9.txt",header = None )
 #df = pd.concat([df1, df2], ignore_index = None)
@@ -13,21 +16,26 @@ import seaborn as sns
 #df1 = df1["maxscore"]
 #%%
 def fc_maxscore (allfiles):
+    count = 0
+    start = timeit.default_timer()
     result = []
-    for file in allfiles:
-        df = pd.read_csv(file,index_col=None, header = None)
-        df["maxscore"] = df.ix[:,:].max(axis = 1)
-        df["meanscore"] = df.ix[:,:-1].mean(axis = 1)
-        df["sumscore"] = df.ix[:,:-2].sum(axis =1)
-        df = df[["maxscore", "meanscore", "sumscore"]]
-        if (len(result) ==0):
-            result = df
-        else:
-            result = pd.concat([df, result], ignore_index = None)
-    result.to_csv("/data/output_Lanh/Final_score/" +"max_mean_sum00"+ ".csv", index = False)
+    for myfile in allfiles:
+        for df in pd.read_csv(myfile, index_col=None, header = None, chunksize=CHUNKSIZE):
+            count = count + 1
+      
+            df["maxscore"] = df.ix[:,:].max(axis = 1).round(3)
+            df["meanscore"] = df.ix[:,:-1].mean(axis = 1)
+            df["sumscore"] = df.ix[:,:-2].sum(axis =1)
+            df = df[["maxscore", "meanscore", "sumscore"]]
+            if (len(result) ==0):
+                result = df
+            else:
+                result = pd.concat([df, result], ignore_index = None)
+    result.to_csv("/Users/sondinh/Downloads/result/sum/result.csv", index = False)
+    stop = timeit.default_timer()
+    print count
+    print stop - start
+
 #%% 
-start = timeit.default_timer()        
 fc_maxscore(allFiles)
-stop = timeit.default_timer()
-print stop- start
 print "Finish"
