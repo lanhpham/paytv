9f08e53f426cc97af1ef49dc7a1997add95e1d2c
lanhpham <honglanhtoantin09@gmail.com>
2016-08-09 22:26:40 +0700

    Write with append mode

--- Giam_roi_mang/calculate_score.py
+++ Giam_roi_mang/calculate_score.py
@@ -3,11 +3,14 @@ import pandas as pd
 import glob
 import timeit 
 import matplotlib.pyplot as plt
+import os
+
 #import seaborn as sns
 #%%
 #define variable
 CHUNKSIZE = 1000
 path =r'/Users/sondinh/Downloads/result'
+output_path = '/Users/sondinh/Downloads/result/sum/result1.csv'
 allFiles = glob.glob(path + "/*.txt")
 #%%
 #df2 = pd.read_csv("/data/output_Lanh/Group/result9.txt",header = None )
@@ -18,20 +21,21 @@ allFiles = glob.glob(path + "/*.txt")
 def fc_maxscore (allfiles):
     count = 0
     start = timeit.default_timer()
-    result = []
     for myfile in allfiles:
+        
         for df in pd.read_csv(myfile, index_col=None, header = None, chunksize=CHUNKSIZE):
+            
             count = count + 1
       
             df["maxscore"] = df.ix[:,:].max(axis = 1).round(3)
             df["meanscore"] = df.ix[:,:-1].mean(axis = 1)
             df["sumscore"] = df.ix[:,:-2].sum(axis =1)
-            df = df[["maxscore", "meanscore", "sumscore"]]
-            if (len(result) ==0):
-                result = df
-            else:
-                result = pd.concat([df, result], ignore_index = None)
-    result.to_csv("/Users/sondinh/Downloads/result/sum/result.csv", index = False)
+            result = df[["maxscore", "meanscore", "sumscore"]]
+            # if file does not exist write header 
+            if not os.path.isfile(output_path):
+               result.to_csv(output_path, index = False)
+            else: # else it exists so append without writing the header
+                result.to_csv(output_path, mode = 'a',index = False)
     stop = timeit.default_timer()
     print count
     print stop - start
