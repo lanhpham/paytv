390ebf4a726951f398cc811ca912359f0b969e7c
lanhpham <honglanhtoantin09@gmail.com>
2016-08-09 15:19:26 +0700

    visualize

--- Giam_roi_mang/RejectUAct_maxscore.py
+++ Giam_roi_mang/RejectUAct_maxscore.py
@@ -2,46 +2,8 @@
 ## Loai UserActive max_score >=0.8
 ## Visualine tập data vector days mới
 import pandas as pd
-import glob
-import timeit 
-path =r'/data/output_Lanh/Group'
-allFiles = glob.glob(path + "/*.txt")
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%%
-#df2 = pd.read_csv("/data/output_Lanh/Group/result9.txt",header = None )
-#df = pd.concat([df1, df2], ignore_index = None)
-#df1["maxscore"] = df1.ix[:,:].max(axis =1)
-#df1 = df1["maxscore"]
-#%%
-def fc_maxscore (allfiles):
-    result = []
-    for file in allfiles:
-        start = timeit.default_timer() 
-        tmp = start
-        df = pd.read_csv(file,index_col=None, header = None)
-        print "load file " + str(timeit.default_timer()- start)
-        start = timeit.default_timer() 
-        df["maxscore"] = df.ix[:,:].max(axis = 1)
-        df["meanscore"] = df.ix[:,:-1].mean(axis = 1)
-        df["sumscore"] = df.ix[:,:-2].sum(axis =1)
-        df = df[["maxscore", "meanscore", "sumscore"]]
-        print "tinh toan" + str(timeit.default_timer()- start)
-        start = timeit.default_timer()
-        if (len(result) ==0):
-            result = df
-        else:
-            result = pd.concat([df, result], ignore_index = None)
-        print "noi table" + str(timeit.default_timer()- start)
-        df = 0
-        print "total" + str(timeit.default_timer()- tmp)
-    result.to_csv("/data/output_Lanh/Final_score/" +"max_mean_sum00"+ ".csv", index = False)
-#%% 
-#start = timeit.default_timer()        
-fc_maxscore(allFiles)
-#stop = timeit.default_timer()
-#print stop- start
-print "Finish"
 #%%
 df_score = pd.read_csv("/data/output_Lanh/Final_score/max_mean_sum.csv")
 data_t5 = pd.read_csv("/data/tv/bf_only_vectordays/t5.csv")
