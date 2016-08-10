6b965c01943af4c72ecb7f7c28db4f8f877ff96e
lanhpham <honglanhtoantin09@gmail.com>
2016-08-10 10:06:48 +0700

    viet lun ra thanh file

--- Giam_roi_mang/Similarity_days.py
+++ Giam_roi_mang/Similarity_days.py
@@ -5,25 +5,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 import threading
 import timeit
 #%%
-data_t6 = pd.read_csv("/data/user/lanhpth/bf_only_vectordays/t6.csv")
+data_t6 = pd.read_csv("/data/tv/bf_only_vectordays/t6.csv")
 #data_t6.columns.values[1:29]
 data_t6["Sum"] = data_t6.ix[:,1:29].sum(axis = 1)
-data_t6 = data_t6[data_t6.ix[:,1:29].sum(axis = 1) > 0]
-#data_ActT6 = data_t6[data_t6["Churn"] == False]
+data_t6 = data_t6[data_t6.ix[:,1:29].sum(axis = 1) > 0].reset_index()
 data_t6= data_t6.ix[:,1:29]
+
 #%%
-data_t5 = pd.read_csv("/data/user/lanhpth/bf_only_vectordays/t5.csv")
-data_t5["Sum"] = data_t5.ix[:,1:29].sum(axis = 1)
+data_t5 = pd.read_csv("/data/tv/bf_only_vectordays/t5.csv")
 data_t5 = data_t5[data_t5.ix[:,1:29].sum(axis = 1) > 0]
 data_ChurnT5 = data_t5[data_t5["Churn"] == True]
 
-data_t4 = pd.read_csv("/data/user/lanhpth/bf_only_vectordays/t4.csv")
-data_t4["Sum"] = data_t4.ix[:,1:29].sum(axis = 1)
+data_t4 = pd.read_csv("/data/tv/bf_only_vectordays/t4.csv")
 data_t4 = data_t4[data_t4.ix[:,1:29].sum(axis = 1) > 0]
 data_ChurnT4 = data_t4[data_t4["Churn"] == True]
 
-data_t3 = pd.read_csv("/data/user/lanhpth/bf_only_vectordays/t3.csv")
-data_t3["Sum"] = data_t3.ix[:,1:29].sum(axis = 1)
+data_t3 = pd.read_csv("/data/tv/bf_only_vectordays/t3.csv")
 data_t3 = data_t3[data_t3.ix[:,1:29].sum(axis = 1) > 0]
 data_ChurnT3 = data_t3[data_t3["Churn"] == True]
 
@@ -32,9 +29,19 @@ data_Churn  = pd.concat([data_ChurnT5, data_ChurnT3, data_ChurnT4],ignore_index 
 #data_train_days = pd.concat([data_ActT5, data_Churn], ignore_index = True)
 #data_train_days.to_csv("/data/tv/bf_only_vectordays/train_days.csv", index = False)
 #%%
+y = data_Churn.ix[:,1:29]
+data_t6.reset_index(inplace = True)   
+data_t6.drop('index',axis=1,inplace=True) 
+arr = np.empty((0, len(y)), float)
+for i in range(10):
+    x =  data_t6.ix[i:i,:]
+    result = cosine_similarity(x,y)
+    arr = np.append(arr, np.array(result), axis=0)
 #%%
-#for i in range(len(ActT5)):
-#    x =  ActT5.ix[i:i,:]cd --
+#y = data_Churn.ix[:,1:29]
+#arr = np.array([], float)
+#for i in range(100):
+#    x =  data_t6.ix[i:i,:]
 #    result = cosine_similarity(x,y)
 #    arr = np.append(arr, np.array(result), axis=0)
 
@@ -52,7 +59,7 @@ class myThread (threading.Thread):
     def run(self):
         y = self.y
         for i in self.my_range:
-            x =  self.data.ix[i,:]
+            x =  self.data.ix[i:i,:]
             #print cosine_similarity(x,y)
             result = cosine_similarity(x,y)
             self.arr = np.append(self.arr, np.array(result), axis=0)
@@ -60,7 +67,7 @@ class myThread (threading.Thread):
 #        self.cond.notify()
 #        self.cond.release()
         score = pd.DataFrame(data = self.arr[0:,0:])
-        score.to_csv("/data/user/lanhpth/output_Lanh/result_thread" + str(self.index) +  ".txt" ,index = False)
+        score.to_csv("/data/user/lanhpth/output_lanh/result_thread" + str(self.index) +  ".txt" ,index = False)
 start = timeit.default_timer()
 y = data_Churn.ix[:,1:29]
 arr = np.empty((0, len(y)), float)
