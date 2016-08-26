5c213ecf871a7da5fc38565277d38ab2cc0b0fdd
lanhpham <honglanhtoantin09@gmail.com>
2016-08-26 09:23:21 +0700

    using multi processing

--- Giam_roi_mang/imbalances_learn.py
+++ Giam_roi_mang/imbalances_learn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import timeit
 from fastdtw import dtw,fastdtw
 from scipy.spatial.distance import *
+import multiprocessing
 #%%
 out_path = "/data/tv/out_put_Lanh/"
 data_Churn = pd.read_csv("/data/tv/Cluster_Cosine/T6_Clu6/trainChurn_0.csv")
@@ -19,41 +20,39 @@ for j in range(2):
         temp[0,i] = distance
 result = np.append(result, np.array(temp), axis=1)
 #%%
-class myThread (threading.Thread):
+class myThread (multiprocessing.Process):
     def __init__(self, my_range, data, index):
-        threading.Thread.__init__(self)
+        super(myThread, self).__init__()
         self.my_range = my_range
         self.data = data
         self.index = index
     def run(self):
-        result = np.empty((len(self.my_range), len(self.data)))
+        result = np.empty(shape = [len(self.my_range), len(self.data)])
         for i in self.my_range:
             x =  self.data.ix[i,:]
             x = np.array(x).reshape(1,-1)
-            temp = np.empty(shape = [1, len(self.data)])
             for j in range(len(self.data)+1):
                 y =  self.data.ix[i,:]
                 y = np.array(x).reshape(1,-1)
                 distance = dtw(x, y,dist= cosine)[0]
-                temp[i,j] = distance
-            result = np.append(result, np.array(temp), axis=1)
+                result[i,j] = distance
         score = pd.DataFrame(data = result)
         score.to_csv(out_path +str(self.index).zfill(3) + "thread.txt", header = None, index = False)
+if __name__ == '__main__':   
+    start = timeit.default_timer()
+    length_data = range(100)
+    #length_data = range(len(data_Churn))
+    data_split = np.array_split(length_data,100)
     
-start = timeit.default_timer()
-length_data = range(100)
-#length_data = range(len(data_Churn))
-data_split = np.array_split(length_data,100)
-
-threads = []
-for index, item in enumerate(data_split, start=0):
-    threads.append(myThread(item, data_Churn, index))
-
-for t in threads:
-    t.start()
-for t in threads:
-    t.join()
-stop = timeit.default_timer()
-        
-print stop - start
-print "Exiting Main Thread"
+    threads = []
+    for index, item in enumerate(data_split, start=0):
+        threads.append(myThread(item, data_Churn, index))
+    
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    stop = timeit.default_timer()
+            
+    print stop - start
+    print "Exiting Main Thread"
