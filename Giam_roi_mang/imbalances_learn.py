17472864237a123b767ee785d55c8a45316eb345
lanhpham <honglanhtoantin09@gmail.com>
2016-08-26 09:34:13 +0700

    Update code

--- Giam_roi_mang/imbalances_learn.py
+++ Giam_roi_mang/imbalances_learn.py
@@ -4,21 +4,7 @@ import timeit
 from fastdtw import dtw,fastdtw
 from scipy.spatial.distance import *
 import multiprocessing
-#%%
-out_path = "/data/tv/out_put_Lanh/"
-data_Churn = pd.read_csv("/data/tv/Cluster_Cosine/T6_Clu6/trainChurn_0.csv")
-data_Churn = data_Churn.ix[:,1:-2]
-#%%
-result = np.empty((2, len(data_Churn)))
-for j in range(2):
-    temp = np.empty(shape = [1, len(data_Churn)])
-    x = data_Churn.ix[j,:]
-    x = np.array(x).reshape(1,-1)
-    for i in range(len(data_Churn)):
-        y = np.array(data_Churn.ix[i,:]).reshape(1,-1)
-        distance = dtw(x, y,dist= cosine)[0]
-        temp[0,i] = distance
-result = np.append(result, np.array(temp), axis=1)
+
 #%%
 class myThread (multiprocessing.Process):
     def __init__(self, my_range, data, index):
@@ -40,6 +26,9 @@ class myThread (multiprocessing.Process):
         score.to_csv(out_path +str(self.index).zfill(3) + "thread.txt", header = None, index = False)
 if __name__ == '__main__':   
     start = timeit.default_timer()
+    out_path = "/data/tv/out_put_Lanh/"
+    data_Churn = pd.read_csv("/data/tv/Cluster_Cosine/T6_Clu6/trainChurn_0.csv")
+    data_Churn = data_Churn.ix[:,1:-2]
     length_data = range(100)
     #length_data = range(len(data_Churn))
     data_split = np.array_split(length_data,100)
