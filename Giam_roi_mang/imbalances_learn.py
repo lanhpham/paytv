b7d4ae59a5247191312e1274ae8e8c14afc2a3b9
lanhpham <honglanhtoantin09@gmail.com>
2016-08-26 09:08:00 +0700

    tinh dtw

--- Giam_roi_mang/imbalances_learn.py
+++ Giam_roi_mang/imbalances_learn.py
@@ -4,45 +4,49 @@ import timeit
 from fastdtw import dtw,fastdtw
 from scipy.spatial.distance import *
 #%%
-out_path = "/data/tv/output_Lanh/"
+out_path = "/data/tv/out_put_Lanh/"
 data_Churn = pd.read_csv("/data/tv/Cluster_Cosine/T6_Clu6/trainChurn_0.csv")
 data_Churn = data_Churn.ix[:,1:-2]
 #%%
-#temp = np.empty(shape = [1, len(data_Churn)])
-#x = data_Churn.ix[0,1:-2]
-#x = np.array(x).reshape(1,-1)
-#y = np.array(data_Churn.ix[:,])
-#y =np.array(y).reshape(1,-1)
-#t = dtw(x, y,dist= cosine)[0]
+for j in range(2):
+temp = np.empty(shape = [1, len(data_Churn)])
+x = data_Churn.ix[0,:]
+x = np.array(x).reshape(1,-1)
+for i in range(len(data_Churn)):
+    y = np.array(data_Churn.ix[i,:]).reshape(1,-1)
+    distance = dtw(x, y,dist= cosine)[0]
+    temp[0,i] = distance
+
 #%%
 class myThread (threading.Thread):
-    def __init__(self, my_range, data,y, index):
+    def __init__(self, my_range, data, index):
         threading.Thread.__init__(self)
         self.my_range = my_range
         self.data = data
-        self.y = y
         self.index = index
     def run(self):
-        temp = np.empty(shape = [self.len(my_shape), self.len(my_shape)])
-        y = self.y
+        result = np.empty((len(self.my_range), len(self.data)))
         for i in self.my_range:
             x =  self.data.ix[i,:]
             x = np.array(x).reshape(1,-1)
-            distance = dtw(x, y,dist= cosine)[0]
-            score = pd.DataFrame(data = distance[0:,0:])
-            score.to_csv(out_path +str(self.index).zfill(3) + "thread.txt", mode = "a", header = None, index = False)
+            temp = np.empty(shape = [1, len(self.data)])
+            for j in range(len(self.data)+1):
+                y =  self.data.ix[i,:]
+                y = np.array(x).reshape(1,-1)
+                distance = dtw(x, y,dist= cosine)[0]
+                temp[i,j] = distance
+            result = np.append(result, np.array(temp), axis=1)
+        score = pd.DataFrame(data = result)
+        score.to_csv(out_path +str(self.index).zfill(3) + "thread.txt", header = None, index = False)
     
 start = timeit.default_timer()
-y = np.array(data_Churn.ix[:,:])
-
-
 length_data = range(100)
 #length_data = range(len(data_Churn))
 data_split = np.array_split(length_data,100)
 
 threads = []
 for index, item in enumerate(data_split, start=0):
-    threads.append(myThread(item, data_Churn, y, index))
+    threads.append(myThread(item, data_Churn, index))
 
 for t in threads:
     t.start()
